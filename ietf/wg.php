b66ddccf9798244349bcade3624bc2e6c9225e01
Eric Vyncke <eric@vyncke.org>
2026-06-26 10:49:00 +0200

    Use ietf directory

--- ietf/wg.php
+++ ietf/wg.php
@@ -1,5 +1,5 @@
 <?php
-# Copyright 2022 Eric Vyncke, evyncke@cisco.com
+# Copyright 2022-2026 Eric Vyncke, evyncke@cisco.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,18 +14,19 @@
 #     limitations under the License.
 #
 # HTTP/2 push of CSS via header()
-header('Link: </ietf/participants.js>;rel=preload;as=script,</ietf/wg.js>;rel=preload;as=script,</ietf/utils.js>;rel=preload;as=script,</ietf/wgchairs.js>;rel=preload;as=script,</ietf/meetings.js>;rel=preload;as=script') ;
+header('Link: </participants2.js>;rel=preload;as=script,</wg.js>;rel=preload;as=script,</utils.js>;rel=preload;as=script,</wgchairs.js>;rel=preload;as=script,</meetings.js>;rel=preload;as=script') ;
 ?><!doctype html>
 <html lang="en">
 <head>
-<title>IETF Participants and COVID-19</title>
+<title>IETF WG Participants</title>
 <!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <!--- get all IETF participants onsite + per country statistics + date of collection -->
-	<script type="text/javascript" src="participants.js"></script>
+<!-- TODO handle past IETF meetings -->
+	<script type="text/javascript" src="participants2.js"></script>
 	<script type="text/javascript" src="wg.js"></script>
 	<script type="text/javascript" src="wgchairs.js"></script>
 	<script type="text/javascript" src="meetings.js"></script>
@@ -69,13 +70,22 @@ participantsCount = 0 ;
 participantsOnSiteCount = 0 ; 
 participantsRemoteCount = 0 ; 
 participantsUnknownCount = 0 ;
+participantsOnsiteNames = [] ;
+participantsRemoteNames = [] ; 
+participantsUnknownNames = [] ;
 
-function displayCategory(elemId, name, onsite, remote, unknown, total) {
+// Find a participants based on the datatracker ID
+function findParticipantById(id, table) {
+        if (table[id]) return true ;
+        return false ;
+}
+
+function displayCategory(elemId, name, onsite, remote, unknown, total, onsiteNames = null, remoteNames = null, unknownNames = null) {
 	var leaders = document.getElementById(elemId) ;
 	leaders.innerHTML = '<p>Out of the ' + total + ' ' + name + ', there are:</p><ul>' +
-		'<li>' + onsite + ' on site;</li>' +
-		'<li>' + remote + ' remote;</li>' +
-		'<li>' + unknown + ' not registered yet (or <abbr title="no correlation was possible between registration and datatracker data (different writing of the first & last names)">not found</abbr>).</li>' +
+		'<li><abbr title="' + onsiteNames + '">' + onsite + ' on site</abbr>;</li>' +
+		'<li><abbr title="' + remoteNames + '">' + remote + ' remote</abbr>;</li>' +
+		'<li><abbr title="' + unknownNames + '">' + unknown + ' not registered yet </abbr>(or <abbr title="no correlation was possible between registration and datatracker data (different writing of the first & last names)">not found</abbr>).</li>' +
 		'</ul>' ;
 	document.getElementById(elemId + 'Onsite').style.width = Math.round(100.0*onsite/total) + "%" ;
 	document.getElementById(elemId + 'Onsite').innerHTML = Math.round(100.0*onsite/total) + "%" ;
@@ -88,13 +98,17 @@ function analyseLine(value, index) {
 	if (value == '') return ; // Somes times empty lines...
 	var name = value.split("\t")[0] ;
 	participantsCount ++ ;
-	if (findParticipant(name, participantsOnsite)) {
-		console.log(name, " is on site") ;
+	if (findParticipantByName(name, participantsOnsite)) {
+		console.log(name, "is on site") ;
 		participantsOnSiteCount++ ;
-	} else if (findParticipant(name, participantsRemote)) {
+		participantsOnsiteNames.push(name) ;
+	} else if (findParticipantByName(name, participantsRemote)) {
+		console.log(name, "is remote") ;
 		participantsRemoteCount++ ;
+		participantsRemoteNames.push(name) ;
 	} else {
 		participantsUnknownCount++ ;
+		participantsUnknownNames.push(name) ;
 		fuzzyMatch(name) ;
 	}
 }
@@ -105,7 +119,8 @@ function analyseBluesheets() {
   lines.forEach(analyseLine) ;
   // Now let's display the outcome
   document.getElementById('participants').style.display = 'block' ;
-  displayCategory('participants', 'expected attendees', participantsOnSiteCount, participantsRemoteCount, participantsUnknownCount, participantsCount) ;
+  displayCategory('participants', 'expected attendees', participantsOnSiteCount, participantsRemoteCount, participantsUnknownCount, participantsCount,
+		participantsOnsiteNames.join(', '), participantsRemoteNames.join(', '), participantsUnknownNames.join(', '))
   document.getElementById('participantsProgressBar').style.display = 'flex' ;
 }
 
@@ -118,9 +133,9 @@ function checkWGLeaders(wgName, elem) {
 			if (wgRole[0] != wgName)
 				continue ;
 			msg += '<li>' + wgChairs[p].name + ' (' + wgRole[1] + '): ' ;
-			if (findParticipant(wgChairs[p].name, participantsOnsite) || findParticipant(wgChairs[p].ascii_name, participantsOnsite))
+			if (findParticipantById(p, participantsOnsite) || findParticipantByName(wgChairs[p].name, participantsOnsite) || findParticipantByName(wgChairs[p].ascii_name, participantsOnsite))
 				msg += 'onsite;' ;
-			else if (findParticipant(wgChairs[p].name, participantsRemote) || findParticipant(wgChairs[p].ascii_name, participantsRemote))
+			else if (findParticipantById(p, participantsRemote) ||findParticipantByName(wgChairs[p].name, participantsRemote) || findParticipantByName(wgChairs[p].ascii_name, participantsRemote))
 				msg += 'remote;' ;
 			else
 				msg += 'not found on registration;' ;
@@ -141,6 +156,9 @@ function onChange(elem) {
 	participantsOnSiteCount = 0 ; 
 	participantsRemoteCount = 0 ; 
 	participantsUnknownCount = 0 ;
+	participantsOnsiteNames = [] ;
+	participantsRemoteNames = [] ; 
+	participantsUnknownNames = [] ;
 	// Check whether this is a valid WG
 	if (! wgs[elem.value]) {
 		console.log('Invalid name', elem.value) ;
@@ -156,9 +174,10 @@ function onChange(elem) {
 		document.getElementById('resultText').insertAdjacentHTML('beforeend', '<p style="color: red;">Alas, no blue sheets are available for the previous WG meeting...</p>') ;
 		return ;
 	}
-	var uri = "https://www.ietf.org/proceedings/" + lastMeeting + "/bluesheets/" + bluesheetsFilename ;
-	document.getElementById('resultText').insertAdjacentHTML('beforeend', '<p><em>Fetching <a href="' + uri + '">blue sheets</a> of the latest WG meeting.</em></p>') ;
-	var response = fetch(uri) // fetch() returns a 'Promise' object
+	var uri = "/proceedings/" + lastMeeting + "/bluesheets/" + bluesheetsFilename ;
+	var proxiedUri = "dt_proxy.php?url=" + encodeURIComponent(uri) ;
+	document.getElementById('resultText').insertAdjacentHTML('beforeend', '<p><em>Fetching <a href="https://www.ietf.org' + uri + '">blue sheets</a> of the last WG meeting.</em></p>') ;
+	var response = fetch(proxiedUri) // fetch() returns a 'Promise' object
 	        // Could use .catch(error =>) to handle errors
 		.then(response => response.text()) // now we have a 'Response' object
 		.then(data => {
@@ -173,13 +192,13 @@ function onChange(elem) {
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-    _paq.push(["setCookieDomain", "*.ehealth.vyncke.org"]);
+    _paq.push(["setCookieDomain", "*.ietf.vyncke.org"]);
     _paq.push(['trackPageView']);
       _paq.push(['enableLinkTracking']);
       (function() {
 	          var u="//analytics.vyncke.org/";
 		      _paq.push(['setTrackerUrl', u+'matomo.php']);
-		      _paq.push(['setSiteId', '6']);
+		      _paq.push(['setSiteId', '9']);
 		          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 		          g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 			    })();
@@ -210,13 +229,11 @@ Select an IETF Working Group:
 </div> <!-- row -->
 <hr>
 <?=$ipv4only_message?>
-<p>If you want to know more on how IETF technologies are used worldwide for "COVID-19 certificates", here are a <a href="https://ehealth.vyncke.org">decoder and explanations</a>.<br/>
+<p>
 <em>Registration data collected on <span id="registrationDate"></span> (hourly refresh),  WG/BoF as of <span id="wgsDate"></span> (daily refresh),
- <a href="https://datatracker.ietf.org/api/">IETF data tracker</a> data,
-and <a href="https://ourworldindata.org/">https://ourworldindata.org/</a>,
-itself based on <a href="https://github.com/CSSEGISandData/COVID-19">JHU CSSE COVID-19 Data</a>.
+ <a href="https://datatracker.ietf.org/api/">IETF data tracker</a> data.
 The power of open data!</em><br/>
-<small>Code is open source and store on IPv4-only github <a href="https://github.com/evyncke/ehealth/tree/main/ietf">repo</a>.</small></p>
+<small>Code is open source and stores on IPv4-only github <a href="https://github.com/evyncke/ehealth/tree/main/ietf">repo</a>.</small></p>
 <!-- Matomo Image Tracker and warning about JS requirement -->
 <noscript><img referrerpolicy="no-referrer-when-downgrade" src="https://analytics.vyncke.org/matomo.php?idsite=6&amp;rec=1" style="border:0" alt="" />
 <b>This site requires javascript.</b></noscript>
@@ -225,3 +242,4 @@ The power of open data!</em><br/>
 <!-- Bootstrap bundle -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+</html>
